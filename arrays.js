6f8bb6b93f16d8d7349dd107448ae19602d3673c
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-15 09:21:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(arr[],n){
+  arr2=["hello",...addElementToBeginningOfArray];
+  return arr2;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr[],n){
+  arr=["hello",...addElementToBeginningOfArray];
+  return arr;
+}
