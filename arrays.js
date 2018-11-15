4bac0e886986ca427e7d50b8ac032a17e24df5b8
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-15 09:21:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr[],n){
   arr2=["hello",...addElementToBeginningOfArray];
